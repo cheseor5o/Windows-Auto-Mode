4aad57730ddb7cbf9ef8fcdd5eebe5dd7e1995cd
Cheese <cheesewky@outlook.com>
2021-05-20 16:28:17 +0800

    Windows Auto Mode v1.0
    -windows系统自动更改深浅模式小程序

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import os
+import time
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+appDarkCmd = 'start /b reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f'
+appLightCmd = 'start /b reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f'
+sysDarkCmd = 'start /b reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 0 /f'
+sysLightCmd = 'start /b reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 1 /f'
 
+current_status = "Dark"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    with open("setting.json", "r") as f:
+        data = json.load(f)
+    current_time = int(time.strftime("%H%M"))
+    light_time = int(time.strftime("%H%M", time.strptime(data["light-time"], "%H:%M")))
+    dark_time = int(time.strftime("%H%M", time.strptime(data["dark-time"], "%H:%M")))
+    # Light
+    if light_time < current_time < dark_time:
+        if current_status == "Light":
+            pass
+        else:
+            current_status = "Light"
+            if data["sys-light"] is True: os.system(sysLightCmd)
+            if data["app-light"] is True: os.system(appLightCmd)
+        print("Light Mode")
+    # Dark
+    else:
+        if current_status == "Dark":
+            pass
+        else:
+            current_status = "Dark"
+            if data["sys-dark"] is True: os.system(sysDarkCmd)
+            if data["app-dark"] is True: os.system(appDarkCmd)
+        print("Dark Mode")
+    time.sleep(60)
